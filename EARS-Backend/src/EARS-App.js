7b2b23f2f44f93c44561fdd73656641392e321f3
Andresinho <rojassanchezedwinandres@gmail.com>
2026-05-02 23:21:40 -0500

    Aqui modifiqué el archivo ya que añadí lo que eliminé en server.js

--- EARS-Backend/src/EARS-App.js
+++ EARS-Backend/src/EARS-App.js
@@ -6,5 +6,9 @@ export const EARS_App = express();
 EARS_App.use(cors());
 EARS_App.use(express.json())
 
-import { EARS_RutaPersona } from './modules/EARS_Persona/EARS_Persona.routes';
-EARS_App.use('api/persona', EARS_RutaPersona)
+import { EARS_RutaPersona } from './modules/EARS_Persona/EARS_Persona.routes.js';
+EARS_App.use('/api/persona', EARS_RutaPersona)
+
+EARS_App.listen(process.env.PORT, () => {
+    console.log('Server corriendo en puerto:', process.env.PORT)
+});
