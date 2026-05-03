c74c0d9eb67be17d27af352b4ba9ac0aa44d870c
Andresinho <rojassanchezedwinandres@gmail.com>
2026-05-02 23:20:10 -0500

    Decidí eliminar este archivo sobre la conexion con el puerto 3000 y lo agregué en App.js

--- EARS-Backend/src/EARS-Server.js
+++
@@ -1,8 +0,0 @@
-import { EARS_App } from "./EARS-App";
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-EARS_App.listen(process.env.PORT, () => {
-    console.log('Server corriendo en puerto:', process.env.PORT)
-});
